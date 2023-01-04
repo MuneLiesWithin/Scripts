dbbf748406a70d897497f4c8ff02286249b13c95
MuneLiesWithin <romulo_medeiros@hotmail.com>
2023-01-03 21:53:20 -0300

    update

--- htmlparser.sh
+++ htmlparser.sh
@@ -4,11 +4,11 @@ then
 	echo "Modo de uso: $0 URL"
 	echo "Exemplo: $0 globo.com"
 else
-	wget -q $1
+	wget -4 -q $1
 	cat index.html | grep "href" | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v ">" | sort -u > lista.txt
-	for url in $(cat lista.txt);
+	for url in $(cat lista.txt | grep ".com");
 	do
-		host $url | grep "has address" | sed "s/has address/=/";
+		host -4 $url | grep "has address" | sed "s/has address/=/";
 	done    
 	rm index.html lista.txt
 fi
